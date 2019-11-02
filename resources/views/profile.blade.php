b1552d890946f1bb61a77bee63f3d2af8a20c45e
Afro-Coder <leon9923@gmail.com>
2019-11-02 15:26:51 +0530

    fixed jquery-easing

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -280,7 +280,7 @@
 			  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
 			  crossorigin="anonymous"></script> --}}
 
-  <script type="text/javascript" src="{{asset('js/bootstrap.bundle.min.js')}}"></script>
+  <script type="text/javascript" src="{{asset('js/bootstrap.bundle.js')}}"></script>
   {{-- <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script> --}}
 
 
